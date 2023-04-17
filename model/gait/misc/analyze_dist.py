d93f18ef60951b2ce2a8bf246aaea9b8182d28d6
jackhanyuan <jackhanyuan@foxmail.com>
2023-04-17 20:33:16 +0800

    update analyze_dist

--- model/gait/misc/analyze_dist.py
+++ model/gait/misc/analyze_dist.py
@@ -245,8 +245,8 @@ def draw_distribution_histogram(nums, save_path, xlabel, ylabel, title, stat="co
     sns.histplot(data=nums, stat=stat, bins=bins, binwidth=binwidth, binrange=binrange,
                  kde=is_kde, cumulative=is_cumulative, line_kws=dict(), color="steelblue")
     
-    # 设置字体
-    plt.rcParams['font.sans-serif'] = 'times new roman'
+    # 设置
+    # plt.rcParams['font.sans-serif'] = 'times new roman'
     plt.xlim(xmin=0, xmax=26)
     plt.xticks(range(0, 28, 2))
     
